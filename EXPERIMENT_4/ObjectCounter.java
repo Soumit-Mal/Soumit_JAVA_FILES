33ff3d5380967da6b7a07d3659dc9cdf41dcee3d
Soumit-Mal <soumitmal10@gmail.com>
2025-02-09 00:22:30 +0530

    Update ObjectCounter.java

--- EXPERIMENT_4/ObjectCounter.java
+++ EXPERIMENT_4/ObjectCounter.java
@@ -1,18 +1,19 @@
-class ObjectCounter {
-    private static int count = 0;
+class Counter {
+    static int count = 0; 
 
-    public ObjectCounter() {
+    Counter() {
         count++;
     }
 
-    public static int getCount() {
-        return count;
+    static void displayCount() {
+        System.out.println("Number of objects created: " + count);
     }
 
     public static void main(String[] args) {
-        ObjectCounter obj1 = new ObjectCounter();
-        ObjectCounter obj2 = new ObjectCounter();
-        ObjectCounter obj3 = new ObjectCounter();
-        System.out.println("Number of objects created: " + ObjectCounter.getCount());
+        Counter obj1 = new Counter();
+        Counter obj2 = new Counter();
+        Counter obj3 = new Counter();
+        
+        Counter.displayCount();
     }
 }
